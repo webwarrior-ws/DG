9490e8a8262c0355b4abc90f81aed8a7f2b95ad1
siwatanejo <andrew.forsure@gmail.com>
2023-05-09 11:14:49 +0800

    scripts,GitHubCI: prepare for release creation
    The APK would be uploaded as part of a GitHub release.
    The bump.fsx is taken from geewallet.
    
    The reason to put the sanity check before the builds is twofold:
    * It saves build time in case it fails.
    * It workarounds a possible dotnet bug[1] related to mutexes
    when `sudo dotnet` is used first and `dotnet` used afterwards.
    
    ```
    Run dotnet fsi scripts/versionSanityCheck.fsx
    System.IO.IOException: The system cannot open the device or file specified. : 'NuGet-Migrations'
       at System.Threading.Mutex.CreateMutexCore(Boolean initiallyOwned, String name, Boolean& createdNew)
       at System.Threading.Mutex..ctor(Boolean initiallyOwned, String name)
       at NuGet.Common.Migrations.MigrationRunner.Run()
       at Microsoft.DotNet.Configurer.DotnetFirstTimeUseConfigurer.Configure()
       at Microsoft.DotNet.Cli.Program.ConfigureDotNetForFirstTimeUse(IFirstTimeUseNoticeSentinel firstTimeUseNoticeSentinel, IAspNetCertificateSentinel aspNetCertificateSentinel, IFileSentinel toolPathSentinel, Boolean isDotnetBeingInvokedFromNativeInstaller, DotnetFirstRunConfiguration dotnetFirstRunConfiguration, IEnvironmentProvider environmentProvider, Dictionary`2 performanceMeasurements)
       at Microsoft.DotNet.Cli.Program.ProcessArgs(String[] args, TimeSpan startupTime, ITelemetry telemetryClient)
       at Microsoft.DotNet.Cli.Program.Main(String[] args)
    ```
    
    [1] https://github.com/dotnet/runtime/issues/80619#issuecomment-1460604725

---
+++ scripts/bump.fsx
@@ -0,0 +1,203 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System
+open System.Linq
+open System.IO
+
+#r "nuget: Fsdk, 0.6.0--date20230502-0946.git-f0c1aee"
+
+open Fsdk
+open Fsdk.Process
+
+let rootDir = DirectoryInfo(Path.Combine(__SOURCE_DIRECTORY__, ".."))
+
+let IsStable miniVersion =
+    (int miniVersion % 2) = 0
+
+let args = Misc.FsxOnlyArguments()
+
+let suppliedVersion =
+    if args.Length > 0 then
+        if args.Length > 1 then
+            Console.Error.WriteLine "Only one argument supported, not more"
+            Environment.Exit 1
+            failwith "Unreachable"
+        else
+            let full = (Version args.Head)
+
+            if not(IsStable full.Build) then
+                Console.Error.WriteLine
+                    "Mini-version (previous-to-last number, e.g. 2 in 0.1.2.3) should be an even (stable) number"
+
+                Environment.Exit 2
+                failwith "Unreachable"
+
+            if full.Revision <> 0 then
+                Console.Error.WriteLine
+                    "Revision number (last number, e.g. 3 in 0.1.2.3) should be zero (UWP restrictions...)"
+
+                Environment.Exit 2
+                failwith "Unreachable"
+
+            Some full
+    else
+        None
+
+let filesToBumpMiniVersion: seq<FileInfo> = [] :> seq<FileInfo>
+
+let filesToBumpFullVersion: seq<FileInfo> =
+    Seq.append
+        filesToBumpMiniVersion
+        [
+            Path.Combine(rootDir.FullName, "src/Frontend/Frontend.csproj")
+            |> FileInfo
+            Path.Combine(
+                rootDir.FullName,
+                "src/Frontend/Platforms/iOS/Info.plist"
+            )
+            |> FileInfo
+            Path.Combine(
+                rootDir.FullName,
+                "src/Frontend/Platforms/Android/AndroidManifest.xml"
+            )
+            |> FileInfo
+        ]
+
+let filesToGitAdd: seq<FileInfo> = filesToBumpFullVersion
+
+let Replace file fromStr toStr =
+    Misc.ReplaceTextInFile file fromStr toStr
+
+let Bump(toStable: bool) : Version * Version =
+    let fullVersion = Misc.GetCurrentVersion rootDir
+    let androidVersion = fullVersion.Build // 0.1.2.3 -> 2
+
+    if toStable && IsStable androidVersion then
+        failwith
+            "bump script expects you to be in unstable version currently, but we found a stable"
+
+    if (not toStable) && (not(IsStable androidVersion)) then
+        failwith
+            "sanity check failed, post-bump should happen in a stable version"
+
+    let newFullVersion, newVersion =
+        match suppliedVersion, toStable with
+        | (Some full), true -> full, full.Build
+        | _ ->
+            let newVersion = androidVersion + 1
+
+            let full =
+                Version(
+                    sprintf
+                        "%i.%i.%i.%i"
+                        fullVersion.Major
+                        fullVersion.Minor
+                        newVersion
+                        fullVersion.Revision
+                )
+
+            full, newVersion
+
+    for file in filesToBumpFullVersion do
+        Replace file (fullVersion.ToString()) (newFullVersion.ToString())
+
+    for file in filesToBumpFullVersion do
+        Replace
+            file
+            (sprintf "versionCode=\"%s\"" (androidVersion.ToString()))
+            (sprintf "versionCode=\"%s\"" (newVersion.ToString()))
+
+    fullVersion, newFullVersion
+
+
+let GitCommit (fullVersion: Version) (newFullVersion: Version) =
+    for file in filesToGitAdd do
+        let gitAdd =
+            {
+                Command = "git"
+                Arguments = sprintf "add %s" file.FullName
+            }
+
+        Process.Execute(gitAdd, Echo.Off).UnwrapDefault() |> ignore<string>
+
+    let commitMessage =
+        sprintf
+            "Bump version: %s -> %s"
+            (fullVersion.ToString())
+            (newFullVersion.ToString())
+
+    let finalCommitMessage =
+        if IsStable fullVersion.Build then
+            sprintf "(Post)%s" commitMessage
+        else
+            commitMessage
+
+    let gitCommit =
+        {
+            Command = "git"
+            Arguments = sprintf "commit -m \"%s\"" finalCommitMessage
+        }
+
+    Process
+        .Execute(gitCommit, Echo.Off)
+        .UnwrapDefault()
+    |> ignore<string>
+
+let GitTag(newFullVersion: Version) =
+    if not(IsStable newFullVersion.Build) then
+        failwith
+            "something is wrong, this script should tag only even(stable) mini-versions, not odd(unstable) ones"
+
+    let gitDeleteTag =
+        {
+            Command = "git"
+            Arguments = sprintf "tag --delete %s" (newFullVersion.ToString())
+        }
+
+    Process.Execute(gitDeleteTag, Echo.Off) |> ignore
+
+    let gitCreateTag =
+        {
+            Command = "git"
+            Arguments = sprintf "tag %s" (newFullVersion.ToString())
+        }
+
+    Process
+        .Execute(gitCreateTag, Echo.Off)
+        .UnwrapDefault()
+    |> ignore<string>
+
+let GitDiff() =
+
+    let gitDiff =
+        {
+            Command = "git"
+            Arguments = "diff"
+        }
+
+    let gitDiffProc = Process.Execute(gitDiff, Echo.Off)
+
+    if gitDiffProc.UnwrapDefault().Length > 0 then
+        Console.Error.WriteLine "git status is not clean"
+        Environment.Exit 1
+
+GitDiff()
+
+Console.WriteLine "Bumping..."
+let fullUnstableVersion, newFullStableVersion = Bump true
+GitCommit fullUnstableVersion newFullStableVersion
+GitTag newFullStableVersion
+
+Console.WriteLine(
+    sprintf "Version bumped to %s." (newFullStableVersion.ToString())
+)
+
+Console.WriteLine "Post-bumping..."
+let fullStableVersion, newFullUnstableVersion = Bump false
+GitCommit fullStableVersion newFullUnstableVersion
+
+Console.WriteLine(
+    sprintf
+        "Version bumping finished. Remember to push via `git push origin main && git push origin %s`"
+        (newFullStableVersion.ToString())
+)
